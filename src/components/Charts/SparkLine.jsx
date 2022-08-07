df25da7cb05edee155bbf9017246264f3c06f9b2
rizkyhaksono <mrizkyhakson@gmail.com>
2022-08-07 09:16:03 +0700

    menambahkan orders

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -3,7 +3,26 @@ import { SparklineComponent, Inject, SparklineTooltip } from "@syncfusion/ej2-re
 
 const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
     return (
-        <SparklineComponent id={id} height={height} width={width} lineWidth={1} valueType="Numeric" fill={color} border={{ color: currentColor, width: 2 }} dataSource={data} xName="x" yName="y" type={type}>
+        <SparklineComponent
+            id={id}
+            height={height}
+            width={width}
+            lineWidth={1}
+            valueType="Numeric"
+            fill={color}
+            border={{ color: currentColor, width: 2 }}
+            dataSource={data}
+            xName="x"
+            yName="yval"
+            type={type}
+            tooltipSettings={{
+                visible: true,
+                format: "${x} : data${yval}",
+                trackLineSettings: {
+                    visible: true,
+                },
+            }}
+        >
             <Inject services={[SparklineTooltip]} />
         </SparklineComponent>
     );
